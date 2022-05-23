00d76cc1f68c0df8c272857955ae8559a7d2d6ac
Shoma <shomasdutt@gmail.com>
2022-05-23 16:14:11 +0100

    template parts

--- wp-content/themes/shomatheme/functions.php
+++ wp-content/themes/shomatheme/functions.php
@@ -9,6 +9,7 @@ add_action('wp_enqueue_scripts', 'load_css');
 
 
 function load_js() {
+    wp_enqueue_script('jquery');
     wp_register_script('bootstrap', get_template_directory_uri() . 'js/bootstrap.min.js', 'jquery', false, true);
     wp_enqueue_script('bootstrap');
 }
